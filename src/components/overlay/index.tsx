2ea8d398575b79d707bd42f2eed0b73bc7cc5b6a
valebearzotti <valebearzotti1@gmail.com>
2025-04-02 00:32:47 -0300

    Update Overlay component styles for improved readability and spacing
    - Adjusted positioning and padding for overlay text elements to enhance visibility.
    - Changed font size for better legibility across different screen sizes.

--- src/components/overlay/index.tsx
+++ src/components/overlay/index.tsx
@@ -2,11 +2,11 @@ export const Overlay = () => {
   return (
     <div className="absolute inset-0 z-50 flex items-center justify-center">
       <div className="w-full h-full relative">
-        <div className="absolute top-1 md:top-12 left-1/2 -translate-x-1/2 md:left-auto md:right-12 md:translate-x-0 text-white bg-black w-[274px] lg:w-[264px] text-right font-mono text-sm lg:pl-4 py-2">
+        <div className="absolute top-4 md:top-12 left-1/2 -translate-x-1/2 md:left-auto md:right-12 md:translate-x-0 text-white bg-black w-[274px] lg:w-[264px] text-right font-mono text-xs md:text-sm lg:pl-4 py-1 px-3 md:py-2">
           Vortex is a highly performant, extensible columnar data format
         </div>
 
-        <div className="absolute bottom-1 left-1/2 -translate-x-1/2 md:left-14 md:translate-x-0 text-white bg-black w-[274px] lg:w-[324px] text-left font-mono text-sm lg:px-4 py-2">
+        <div className="absolute bottom-5 md:bottom-1 left-1/2 -translate-x-1/2 md:left-14 md:translate-x-0 text-white bg-black w-[274px] lg:w-[324px] text-left font-mono text-xs md:text-sm lg:px-4 py-1 px-3 md:py-2">
           It&apos;s both a file format and a memory format, enabling compute
           over compressed data
         </div>
