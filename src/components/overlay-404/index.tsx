c6cc772917ebf467068e96449625aeabb6a36cc9
valebearzotti <valebearzotti1@gmail.com>
2025-04-02 14:22:25 -0300

    overlay bg color

--- src/components/overlay-404/index.tsx
+++ src/components/overlay-404/index.tsx
@@ -4,7 +4,7 @@ export const Overlay404 = () => {
   return (
     <div className="absolute inset-0 z-50 flex items-center justify-center">
       <div className="w-full h-full relative">
-        <div className="absolute top-4 md:top-12 left-1/2 -translate-x-1/2 md:left-auto md:right-12 md:translate-x-0 text-white bg-black w-[174px] lg:w-[204px] text-right font-mono text-xs md:text-sm lg:pl-4 py-1 px-3 md:py-2">
+        <div className="absolute top-4 md:top-12 left-1/2 -translate-x-1/2 md:left-auto md:right-12 md:translate-x-0 text-white bg-background w-[174px] lg:w-[204px] text-right font-mono text-xs md:text-sm lg:pl-4 py-1 px-3 md:py-2">
           <Link href="/" className="z-10">
             Go back home
           </Link>
