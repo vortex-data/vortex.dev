e90176cceff2aaae298eb79418db8a33120d72a7
valebearzotti <valebearzotti1@gmail.com>
2025-04-08 12:06:53 -0300

    use proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import { withPlausibleProxy } from "next-plausible";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withPlausibleProxy()({
   /* config options here */
-};
+});
 
 export default nextConfig;
