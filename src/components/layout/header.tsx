bc456a3ef9933cafb36f8cb42d1131cbc99d3fb9
valebearzotti <valebearzotti1@gmail.com>
2025-05-07 11:59:50 -0300

    Update logo model + svgs

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,4 @@
+import LogoMobile from "@/assets/logo-mobile.svg";
 import Logo from "@/assets/logo.svg";
 import Image from "next/image";
 import NextLink from "next/link";
@@ -10,10 +11,20 @@ export const Header = () => {
         href="/"
         className="flex items-center gap-4 dashed-right h-full px-8"
       >
-        <Image src={Logo} alt="Logo" width={44} height={44} />
-        <h1 className="text-4xl font-funnel text-white hidden md:inline-block">
-          Vortex
-        </h1>
+        <Image
+          src={Logo}
+          alt="Vortex Logo"
+          width={44}
+          height={44}
+          className="hidden md:block w-full h-[44px]"
+        />
+        <Image
+          src={LogoMobile}
+          alt="Vortex Logo"
+          width={40}
+          height={40}
+          className="block md:hidden w-full h-[40px]"
+        />
       </NextLink>
       <div className="flex items-center gap-8 flex-1 justify-end md:flex-none dashed-left before:hidden md:before:block h-full px-10">
         <Link
