9d6e9d88446cf9cf86c6643fffd6f7c7b31bd922
Will Manning <will@willmanning.io>
2025-05-15 17:29:56 +0100

    link
    Signed-off-by: Will Manning <will@willmanning.io>

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,10 +7,9 @@ export const Footer = () => {
     <div className="flex flex-col-reverse md:flex-row justify-between items-center m-4 mt-2 md:mt-4 md:m-10 dashed-top dashed-bottom after:hidden md:after:block md:h-[84px]">
       <div className="flex items-center gap-4 dashed-right after:hidden lg:after:block h-full md:pl-2 lg:px-10 py-4 pb-4 md:py-0">
         <span className="text-sm font-mono text-white">
-          Copyright © Vortex a Series of LF Projects, LLC. For web site terms
-          of use, <br /> trademark policy and other project policies please see{" "}
+          Copyright © Vortex a Series of{" "}
           <Link href="https://lfprojects.org" target="_blank">
-            https://lfprojects.org
+            LF Projects, LLC
           </Link>
           .
         </span>
