8afe0e1d82d6887c7d31c4a913ce89bb578bdba8
Andrew Duffy <andrew@a10y.dev>
2025-11-24 16:51:10 -0500

    polish
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,13 +6,8 @@ import { Link } from "../link";
 export const Footer = () => {
   return (
     <div className="flex flex-col m-4 mt-2 md:mt-4 md:m-10">
-      <div className="flex justify-center items-center dashed-top dashed-bottom after:hidden md:after:block md:h-[84px]">
-        <div className="flex items-center gap-4 h-full md:pl-2 lg:px-10 py-4 pb-4 md:py-0">
-          <span className="text-sm font-mono text-white">
-            Incubating @ <Image src={LFLogo} alt="Linux Foundation logo" height={15} />
-          </span>
-        </div>
-        <div className="flex items-center gap-8 h-full lg:px-10">
+      <div className="flex justify-center items-center gap-16 dashed-top dashed-bottom after:hidden md:after:block md:h-[84px]">
+        <div className="flex items-center h-full lg:px-10">
           <div className="flex flex-col md:flex-row items-center gap-2 py-4 md:py-0">
             <span className="text-white font-mono text-sm flex gap-3 items-center">
               <NextLink
@@ -36,6 +31,11 @@ export const Footer = () => {
               </span>
             </span>
           </div>
+        </div>
+        <div className="flex items-center h-full lg:px-10 py-4 pb-4 md:py-0">
+          <span className="text-sm font-mono text-white flex items-center gap-2">
+            Incubating @ <Image src={LFLogo} alt="Linux Foundation logo" height={10} className="inline" />
+          </span>
         </div>
       </div>
       <div className="text-center mt-4">
